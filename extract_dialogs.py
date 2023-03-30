a0618af2e80be600a2e9241c9f87616e305bb23d
Jiayi Zhang <zhangjiayi@inspirai.com>
2023-03-30 11:39:11 +0800

    add birthday

--- extract_dialogs.py
+++ extract_dialogs.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--repo",
-        default="/mnt/data/home/workspace/code/github/AnimeGameData",
+        default="Path/To/AnimeGameData",
         type=str,
-        # required=True,
+        required=True,
         help="data dir",
     )
-    parser.add_argument("--lang", default="JP", type=str, help="language type")
+    parser.add_argument("--lang", default="CHS", type=str, help="language type")
     parser.add_argument(
         "--n_utter",
         default=1000,
@@ -39,10 +39,10 @@ if __name__ == "__main__":
         print(json_file, file=f)
     print("Output avatar at {}".format(output_file))
 
-    # # output dialog
-    # if len(output_dialog_list):
-    #     output_file = os.path.join(output_dir, "dialog_{}.jsonl".format(args.lang))
-    #     with open(output_file, "w", encoding="utf-8") as f:
-    #         for dialog in output_dialog_list:
-    #             print(json.dumps(dialog, ensure_ascii=False), file=f)
-    #     print("Output dialog at {}".format(output_file))
+    # output dialog
+    if len(output_dialog_list):
+        output_file = os.path.join(output_dir, "dialog_{}.jsonl".format(args.lang))
+        with open(output_file, "w", encoding="utf-8") as f:
+            for dialog in output_dialog_list:
+                print(json.dumps(dialog, ensure_ascii=False), file=f)
+        print("Output dialog at {}".format(output_file))
